dc8943bdfc5c2559484712efe08f84120acf983a
Bal <Bal@DESKTOP-NTRO8PG>
2021-03-26 17:48:05 +0100

    2nd homework. The plus is missing, working on it :)

---
+++ hr/src/main/java/hu/webuni/hr/martonBalazs/web/EmployeeTLController.java
@@ -0,0 +1,47 @@
+package hu.webuni.hr.martonBalazs.web;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import hu.webuni.hr.martonBalazs.dto.EmployeeDto;
+import hu.webuni.hr.martonBalazs.model.Employee;
+
+@Controller
+public class EmployeeTLController {
+	
+	//private List<Employee> allEmployees = new ArrayList<>();
+	private List<EmployeeDto> allEmployees = new ArrayList<>();
+	
+	{
+		allEmployees.add(new EmployeeDto(1L, "Gyula", "Maintanance", 20, LocalDateTime.parse("2000-03-11T10:00:00")));
+		allEmployees.add(new EmployeeDto(2L, "Erzsébet", "Scrum master", 800, LocalDateTime.parse("2020-03-11T10:00:00")));
+	}
+	
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("pageTitle", "Employees");
+		return "index";
+	}
+	
+	@GetMapping("/employees")
+	public String listEmployees(Map<String, Object> model) {
+		model.put("employees", allEmployees);
+		model.put("newEmployee", new Employee());
+		//model.put("newEmployee", new EmployeeDto());
+		return "employees";
+	}
+	
+	@PostMapping("/employees")
+	public String addEmployee(EmployeeDto employee) {
+		allEmployees.add(employee);
+		return "redirect:employees";
+	}
+
+}
---
+++ hr/src/main/java/hu/webuni/hr/martonBalazs/web/HrController.java
@@ -0,0 +1,68 @@
+package hu.webuni.hr.martonBalazs.web;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import hu.webuni.hr.martonBalazs.dto.EmployeeDto;
+
+@RestController
+@RequestMapping("/api/employees")
+public class HrController {
+	
+	private Map<Long, EmployeeDto> employees = new HashMap<>();
+	
+	{
+		employees.put(1L, new EmployeeDto(1L, "Balazs", "developer", 5000, LocalDateTime.parse("2000-03-11T10:00:00")));
+		employees.put(2L, new EmployeeDto(2L, "Alajos", "intern", 120, LocalDateTime.parse("2020-01-04T09:00:00")));
+	}
+	
+	
+	@GetMapping
+	public List<EmployeeDto> getAll() {
+		return new ArrayList<> (employees.values());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<EmployeeDto> getByid(@PathVariable long id) {
+		EmployeeDto employeeDto = employees.get(id);
+		if (employeeDto != null) {
+			return ResponseEntity.ok(employeeDto);
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public EmployeeDto createEmployee(@RequestBody EmployeeDto employeeDto) {
+		employees.put(employeeDto.getId(), employeeDto);
+		return employeeDto;
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<EmployeeDto> modifyEmployee(@RequestBody EmployeeDto employeeDto, @PathVariable long id) {
+		if (!employees.containsKey(id)) {
+		return ResponseEntity.notFound().build();
+		}
+		employeeDto.setId(id);
+		employees.put(id, employeeDto);
+		return ResponseEntity.ok(employeeDto);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteEmployee(@PathVariable long id) {
+		employees.remove(id);
+	}
+
+}
