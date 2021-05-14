58ebb22168bf678b60436e039fcea00eed03c028
imre-gabor <gabor@aut.bme.hu>
2021-05-14 23:44:22 +0200

    make modifying methods transactional in VacationService

--- hr/src/main/java/hu/webuni/hr/martonBalazs/service/VacationService.java
+++ hr/src/main/java/hu/webuni/hr/martonBalazs/service/VacationService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import hu.webuni.hr.martonBalazs.Repository.VacationRepository;
 import hu.webuni.hr.martonBalazs.dto.VacationDto;
@@ -17,11 +18,12 @@ public class VacationService {
 	@Autowired
 	VacationRepository vacationRepository;
 	
-	
+	@Transactional
 	public Vacation save(Vacation vacation) {
 		return vacationRepository.save(vacation);
 	}
 	
+	@Transactional
 	public Vacation update(Vacation vacation) {
 		if (vacationRepository.existsById(vacation.getId())) {
 			return vacationRepository.save(vacation);
@@ -38,16 +40,18 @@ public class VacationService {
 		return vacationRepository.findById(id);
 	}
 	
+	@Transactional
 	public void delete(long id) {
 		vacationRepository.deleteById(id);
 	}
 	
+	@Transactional
 	public void setVacationStatus(long id, String status) {
 		Vacation vacation = this.findById(id).orElseThrow();
 		vacation.setAccepted(status.equals("accept") ? true : false);
-		this.update(vacation);
 	}
-	
+
+	@Transactional
 	public void deleteVacation(long id) {
 		Vacation vacation = this.findById(id).orElseThrow();
 		if (vacation.isAccepted() == null) {
@@ -55,12 +59,12 @@ public class VacationService {
 		}
 	}
 
+	@Transactional
 	public void modifyVacation(long id, VacationDto vacationDto) {
 		Vacation vacation = this.findById(id).orElseThrow();
 		if (vacation.isAccepted() == null) {
 			vacation.setStartDate(vacationDto.getStartDate());
 			vacation.setEndDate(vacationDto.getEndDate());
-			this.update(vacation);
 		}
 	}
 }
